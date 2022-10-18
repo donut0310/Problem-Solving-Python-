ca0244be3e26b4761767cc467c5c454b0f345fe8
GeomJa <52377043+donut0310@users.noreply.github.com>
2022-10-18 15:43:37 +0900

    Update 더 맵게_Lv2.py

--- Programmers/highscore_kit/heap/더 맵게_Lv2.py
+++ Programmers/highscore_kit/heap/더 맵게_Lv2.py
@@ -3,7 +3,7 @@ import heapq
 def solution(scoville, K):
     answer = 0
     
-    heapq.heapify(scoville)
+    heapq.heapify(scoville) # 리스트를 힙구조로 변환하지 않고 사용하면 오답 발생
     while len(scoville) > 1:
         first = heapq.heappop(scoville)
         if first >= K: return answer
@@ -14,4 +14,3 @@ def solution(scoville, K):
 
     if scoville[0] >= K: return answer
     return -1
-    
