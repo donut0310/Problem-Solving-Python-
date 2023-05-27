1fd81e9d642e37731fadafa98536cf4d0177d8e0
0RYUL <donut0310@naver.com>
2023-05-27 10:49:26 +0900

    [unrated] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -6,7 +6,8 @@ where car_id in (
     from car_rental_company_rental_history
     where start_date between '2022-08-01' and '2022-10-31'
     group by car_id
-    having count(history_id) >= 5)
-    and start_date between '2022-08-01' and '2022-10-31'
+    having count(history_id) >= 5
+) 
+and start_date between '2022-08-01' and '2022-10-31'
 group by month, car_id
 order by month, car_id desc
